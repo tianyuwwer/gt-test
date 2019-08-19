508ce2a6a1f4c4f0096e4a47c98ef94872a556b2
zhengqunhui <2541985996@qq.com>
2019-08-19 12:04:05 +0800

    提交了修改文件

---
+++ git-test/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("你好啊....");
+        System.out.println("你好啊....");
+        System.out.println("你好啊....");
+        System.out.println("你好啊....");
+        System.out.println("你好啊....");
+        System.out.println("你好啊....");
+
+    }
+
+    public void test(){
+         int i=0;
+        System.out.println(i);
+    }
+}
